c36a12faa6a8d8d9a6008f34ec1b655e21c37a47
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-02 19:11:09 -0300

    Add API functionality with submit and refresh scores

---
+++ src/modules/leaderboardAPI.js
@@ -0,0 +1,19 @@
+const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/DU2APQewjCYKmHf7YWlp/scores';
+
+export const getScores = async () => {
+  const response = await fetch(api);
+  const scores = await response.json();
+  return scores;
+};
+
+export const setScores = async (username, deaths) => {
+  const response = await fetch(api, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user: username, score: deaths }),
+  });
+  const scores = response.json();
+  return scores;
+};
