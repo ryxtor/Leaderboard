8382c1fc56c91dbfd097314a563effcdf5b46422
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-03-04 16:19:49 -0300

    Fix submit btn position

--- src/modules/leaderboardAPI.js
+++ src/modules/leaderboardAPI.js
@@ -1,4 +1,4 @@
-const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/0a1NGfMH4OGvsGgpC5E6/scores';
+const api = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/stsatQaQOZkEyfJMnnPo/scores';
 
 export const getScores = async () => {
   const response = await fetch(api);
